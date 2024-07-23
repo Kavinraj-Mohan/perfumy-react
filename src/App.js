f545184e0571bdfbaebc9c5e7e27674b4e6e5b40
unknown <kavinlinen@gmail.com>
2024-07-24 04:48:17 +0530

    update project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar"
+import SearchBar from "./components/SearchBar"
+import Product from "./components/Product"
+import About from "./components/About"
+import Footer from "./components/Footer"
+ 
+ function App() {
+    return(
+        <div>
+    <Navbar></Navbar>
+    <SearchBar></SearchBar>
+    <Product></Product>
+    <About></About>
+    <Footer></Footer> 
+  
+  </div>
+    )
+ }
+
+ export default App;
